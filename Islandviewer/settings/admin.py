2976e8884a42199b2fea86b998470e68a7f1a745
gemmahoad <ghoad@sfu.ca>
2025-01-27 14:51:26 -0800

    Removed email addresses

--- Islandviewer/settings/admin.py
+++ Islandviewer/settings/admin.py
@@ -1,8 +1,5 @@
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
-    #('James Peltier', 'james_peltier@sfu.ca'),
-    ('Gemma Hoad', 'gemma_hoad@sfu.ca'),
-    ('Vladimir Ortynski', 'vladimir_ortynski@sfu.ca'),
 )
 
 MANAGERS = ADMINS
