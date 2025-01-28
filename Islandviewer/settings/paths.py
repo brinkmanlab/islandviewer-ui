8fdb61504234fbe7fe3b9a4c37e0bb14ce715ef3
gemmahoad <ghoad@sfu.ca>
2025-01-27 16:30:57 -0800

    Made paths more generic

--- Islandviewer/settings/paths.py
+++ Islandviewer/settings/paths.py
@@ -7,8 +7,8 @@ BASE_DIR = os.path.dirname(PROJECT_PATH)
 sys.path.insert(0, os.path.join(BASE_DIR, "lib"))
 
 if env.PROD_ENV:
-    CUSTOM_GENOMES = "/data/transition/islandviewerV4/data/custom_genomes/"
-    GENOME_UPLOAD_PATH = "/data/transition/islandviewerV4/data/custom_genomes/tmp/"
-    GENOME_SUBMISSION_SCRIPT = "/data/transition/islandviewerV4/static/bin/submit_uploaded_genome.pl -c /data/transition/islandviewerV4/static/etc/islandviewer.config -f {filename} -n \"{genome_name}\" -l /data/transition/islandviewerV4/static/etc/logger.upload.conf 2>/dev/null"
-    PIPELINE_PATH = "/data/transition/scheduler/metascheduler/static/etc/pipeline"
-    ANALYSIS_PATH = "/data/transition/islandviewerV4/data/analysis"
+    CUSTOM_GENOMES = "/mypath/islandviewerV4/data/custom_genomes/"
+    GENOME_UPLOAD_PATH = "/mypath/islandviewerV4/data/custom_genomes/tmp/"
+    GENOME_SUBMISSION_SCRIPT = "/mypath/islandviewerV4/static/bin/submit_uploaded_genome.pl -c /mypath/islandviewerV4/static/etc/islandviewer.config -f {filename} -n \"{genome_name}\" -l /mypath/islandviewerV4/static/etc/logger.upload.conf 2>/dev/null"
+    PIPELINE_PATH = "/mypath/scheduler/metascheduler/static/etc/pipeline"
+    ANALYSIS_PATH = "/mypath/islandviewerV4/data/analysis"
